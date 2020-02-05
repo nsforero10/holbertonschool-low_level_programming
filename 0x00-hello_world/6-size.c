6ea272bedf298802fad7ab9af74070dfd6fe1ccd
Nicolas Forero <ns.forero10@gmail.com>
2020-02-05 20:56:10 +0000

    [Feature] Adding 0x00 6 script

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
-
+/**
+ * main - This main function prints size in memory of diferent data types
+ * Return: 0
+ */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,");
-  return(0);
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	printf("Size of a char: %lu byte(s)\n", sizeof(a));
+	printf("Size of a int: %lu byte(s)\n", sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", sizeof(e));
+	return (0);
 }
