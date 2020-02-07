f685ad66c3f17cdbcf164b2fef003832701e8b91
Nicolas Forero <ns.forero10@gmail.com>
2020-02-07 04:19:48 +0000

    [Fix] Fix tabs on 0x01 102 advanced task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,42 +7,42 @@
 */
 int main(void)
 {
-	int i, j, k, l;
+int i, j, k, l;
 
-	i = '0';
-	while (i <= '9')
+i = '0';
+while (i <= '9')
+{
+	j = '0';
+	while (j <= '9')
 	{
-		j = '0';
-		while (j <= '9')
+		k = '0';
+		while (k <= '9')
 		{
-			k = '0';
-			while (k <= '9')
+			l = '0';
+			while (l <= '9')
 			{
-				l = '0';
-				while (l <= '9')
+				if (i < k || (i == k && j < l))
 				{
-					if (i < k || (i == k && j < l))
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (i != '9' || j != '8' ||
+					    k != '9' || l != '9')
 					{
-						putchar(i);
-						putchar(j);
+						putchar(',');
 						putchar(' ');
-						putchar(k);
-						putchar(l);
-						if (i != '9' || j != '8' ||
-						    k != '9' || l != '9')
-						{
-							putchar(',');
-							putchar(' ');
-						}
 					}
-					l++;
 				}
-				k++;
+				l++;
 			}
-			j++;
+			k++;
 		}
-		i++;
+		j++;
 	}
-	putchar('\n');
-	return (0);
+	i++;
+}
+putchar('\n');
+return (0);
 }
