ff0dcd87323e08424a95b169217a71ee2a8cba69
Nicolas Forero <ns.forero10@gmail.com>
2020-02-11 21:25:14 +0000

    [Feature] Adding 0x02 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+*print_sign - checks the sign of a number and prints it
+*@a: number to sum
+*@b: number to sum
+*
+*Return: 1 if is positive, 0 is zero, -1 if is negative
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
