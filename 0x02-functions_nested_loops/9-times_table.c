e16c0a3e91334c9af1ac6b8c25d914aaaab16baa
Nicolas Forero <ns.forero10@gmail.com>
2020-02-11 18:24:30 +0000

    [Feature] Adding 0x02 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ *times_table - print times table
+ *
+ *Return: void
+ */
+void times_table(void)
+{
+	int n, m;
+
+	n = 0;
+	while (n < 10)
+	{
+		m = 0;
+		while (m < 10)
+		{
+			int r = n * m;
+			if(m != 0)
+			{
+				if(r / 10 != 0)
+				{
+					_putchar('0' + r / 10);
+				}
+				else
+					_putchar(' ');
+			}
+			_putchar('0' + r % 10);
+			if(m != 9)
+				_putchar(',');
+			m++;
+		}
+		_putchar('\n');
+		n++;
+	}
+}
