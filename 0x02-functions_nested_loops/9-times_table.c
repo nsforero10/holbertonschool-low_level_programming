715dcb585c20a702da4de49a71a1e718852de2c5
Nicolas Forero <ns.forero10@gmail.com>
2020-02-11 18:29:39 +0000

    [Fix] Fixing 0x02 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,9 +16,9 @@ void times_table(void)
 		while (m < 10)
 		{
 			int r = n * m;
-			if(m != 0)
+			if (m != 0)
 			{
-				if(r / 10 != 0)
+				if (r / 10 != 0)
 				{
 					_putchar('0' + r / 10);
 				}
@@ -26,7 +26,7 @@ void times_table(void)
 					_putchar(' ');
 			}
 			_putchar('0' + r % 10);
-			if(m != 9)
+			if (m != 9)
 				_putchar(',');
 			m++;
 		}
