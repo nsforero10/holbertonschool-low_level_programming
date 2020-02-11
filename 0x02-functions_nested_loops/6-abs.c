e55517c67e9881b9f40431d2fb495dc2998fc5a1
Nicolas Forero <ns.forero10@gmail.com>
2020-02-11 15:07:49 +0000

    [Feature] Adding 6 script

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+ #include "holberton.h"
+ /**
+ * _abs - absolute a number
+ *@n: the number to be absed
+ *
+ *Return: the absolute value of the number
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n + -1);
+	}
+	return (n);
+}
