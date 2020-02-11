efde90fe2c7675ec664e36de0011c9db04335e15
Nicolas Forero <ns.forero10@gmail.com>
2020-02-11 21:34:49 +0000

    [Feature] Adding 0x02 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *print_to_98 - print times table
+ *
+ *
+ *Return: void
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	i = n;
+	if (n < 98)
+	{
+		while (i <= 98)
+		{
+			if (i < 98)
+				printf("%i, ",i);
+			else
+				printf("%i\n",i);
+			i++;
+		}
+	}
+	else
+	{
+		while (i >= 98)
+		{
+			if (i > 98)
+				printf("%i, ",i);
+			else
+				printf("%i\n",i);
+			i--;
+		}
+	}
+}
