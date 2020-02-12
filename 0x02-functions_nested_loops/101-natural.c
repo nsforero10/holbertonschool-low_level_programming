6355f30130c0cab57798a099d406634759b741ac
Nicolas Forero <ns.forero10@gmail.com>
2020-02-12 16:31:29 +0000

    [Fix] Fixing extra additions 0x03 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,9 +10,7 @@ int main(void)
 
 	for (num = 0; num < 1024 ; num++)
 	{
-		if (num % 5)
-			rta += num;
-		if (num % 3)
+		if( num % 5  == 0 || num % 3 == 0)
 			rta += num;
 	}
 	printf("%i \n", rta);
