6d8d5e90346f012b25c226833038c02a1b7bd6aa
Nicolas Forero <ns.forero10@gmail.com>
2020-02-13 22:35:52 +0000

    [Fix] fixing betty 0x04 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,23 +1,21 @@
 #include "holberton.h"
 /**
- *isPrime - x
+ *print_number - x
  *@n: size of the square
  *
- *Return: 1 if is lowercase, 0 otherwise
- *Return: n
+ *Return: void
  */
-#include <stdio.h>
 
 void print_number(int n)
 {
-        unsigned int num = n;
+	unsigned int num = n;
 
-        if (n < 0)
-        {
-                _putchar('-');
-                num = -num;
-        }
-        if ((num / 10) > 0)
-                print_number(num / 10);
-        _putchar((num % 10) + '0');
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
 }
