c3be22b2b29fe136926d0ff374d36f675cf1aecc
Nicolas Forero <ns.forero10@gmail.com>
2020-02-13 18:12:48 +0000

    [Fix] fixing 0 or less case 0x04 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,12 +9,17 @@ void print_square(int n)
 {
 	int i, j;
 
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		for (j = 0; j < n; j++)
+		for (i = 0; i < n; i++)
 		{
-			_putchar('#');
+			for (j = 0; j < n; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
+	else
+		puthar('\n');
 }
