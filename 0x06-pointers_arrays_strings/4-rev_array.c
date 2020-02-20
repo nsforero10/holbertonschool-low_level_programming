192039208bf6a7f01a474abde887cff9e78e5c8e
Nicolas Forero <ns.forero10@gmail.com>
2020-02-20 21:15:34 +0000

    [Fixing] fixing 0x06 4 betty

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,9 +1,10 @@
 #include "holberton.h"
 
 /**
- * reverse_array -
- *
- * Return: 
+ * reverse_array - reverse an array
+ * @s: array to reverse
+ * @n: number of elements to swap
+ * Return: nothing
  */
 void reverse_array(int *s, int n)
 {
@@ -12,7 +13,7 @@ void reverse_array(int *s, int n)
 
 	front = 0;
 	back = n - 1;
-	while (front < back)
+	while (front < back && front <= n)
 	{
 		aux = *(s + front);
 		*(s + front) = *(s + back);
