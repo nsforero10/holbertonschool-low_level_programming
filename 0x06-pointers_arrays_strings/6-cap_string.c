bb320e529904273088194d991b29cefd00bbe5bd
Nicolas Forero <ns.forero10@gmail.com>
2020-02-20 22:35:51 +0000

    [Feature] Adding 0x06 6 betty

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,36 @@
 #include "holberton.h"
 
 /**
- * cap_string -
- *
- * Return: 
+ * cap_string - capitalize certains letters
+ * @str: string to capitilize
+ * Return: pointer to the string
  */
-char *cap_string(char *)
+char *cap_string(char *str)
 {
+	char dic[13] = " \n\t,;.!?\"(){}";
+	unsigned int i, j;
+	char crt;
 
+	i = 0;
+	while (str[i])
+	{
+		crt = str[i];
+		if (i == 0 && crt >= 'a' && crt <= 'z')
+		{
+			crt -= 32;
+		}
+		else
+		{
+			for (j = 0; dic[j]; j++)
+			{
+				if (crt == dic[j])
+				{
+					if (str[i + 1] >= 'a' && str[i + 1])
+						str[i + 1] -= 32;
+				}
+			}
+		}
+		i++;
+	}
+	return (str);
 }
