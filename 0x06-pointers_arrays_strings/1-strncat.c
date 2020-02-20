091ecf215eaa84b82d77609a16d08acf48a31dcb
Nicolas Forero <ns.forero10@gmail.com>
2020-02-20 18:03:46 +0000

    [Fix] fixing  \n  \0 case 0x06 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,7 +14,7 @@ char *_strncat(char *dest, char *src, int n)
 	j = 0;
 	for (i = 0; dest[i] != '\0'; i++)
 	{}
-	while (j < n && src[j] != '\n')
+	while (j < n && src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
