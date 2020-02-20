b328654a538d3889237092ff8718bbe8439b2f03
Nicolas Forero <ns.forero10@gmail.com>
2020-02-20 21:36:04 +0000

    [Feature] Adding 0x06 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,11 +1,21 @@
 #include "holberton.h"
 
 /**
- * string_toupper -
+ * string_toupper - uppercase lowercase chars
  *
- * Return: 
+ *@str: string to uppercase
+ * Return: pointer to the char
  */
-char *string_toupper(char *)
+char *string_toupper(char *str)
 {
+	int i;
 
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] > 'a' && str[i] < 'z')
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
 }
