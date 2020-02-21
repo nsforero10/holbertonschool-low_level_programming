c155338930c52e56c34ddc91f5dcb4c97da51638
Nicolas Forero <ns.forero10@gmail.com>
2020-02-21 00:57:36 +0000

    [Add] Adding 0x06 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,11 +1,30 @@
 #include "holberton.h"
 
 /**
- * leet -
- *
- * Return: 
+ * leet - encodes into 1337
+ * @src: string to be encoded
+ * Return: a pointer to the encoded
  */
-char *leet(char *)
+char *leet(char *src)
 {
+	unsigned int i, j;
+	char dic[10] = "AaEeOoTtLl";
+	char nums[5] = "43071";
 
+	i = 0;
+	while (src[i])
+	{
+		j = 0;
+		while (dic[j])
+		{
+			if (src[i] == dic[j] || src[i] == dic[j + 1])
+			{
+				src[i] = nums[j / 2];
+				break;
+			}
+			j += 2;
+		}
+		i++;
+	}
+	return (src);
 }
