827df1f8854c0c31f2908ca79acdc50dd52e63ba
Nicolas Forero <ns.forero10@gmail.com>
2020-02-21 01:44:13 +0000

    [Add] Adding 0x06 8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,11 +1,28 @@
 #include "holberton.h"
 
 /**
- * rot13 -
- *
- * Return: 
+ * rot13 - encode with rot13
+ * @src: string to be encoded
+ * Return: pointer to string encoded
  */
-char *rot13(char *)
+char *rot13(char *src)
 {
+	int i = 0,j;
+	char dic[52] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char dic2[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
+	while (src[i])
+	{
+		j = 0;
+		while (dic[j])
+		{
+			if (src[i] == dic[j])
+			{
+				src[i] = dic2[j];
+			}
+			j++;
+		}
+		i++;
+	}
+	return (src);
 }
