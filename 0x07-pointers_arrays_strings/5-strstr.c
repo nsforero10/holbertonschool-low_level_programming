ab5a4adc5293adf5b828578c2e10a26e40a8280b
Nicolas Forero <ns.forero10@gmail.com>
2020-02-25 00:04:46 +0000

    [Feature] Adding 0x07 5 task

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,11 +1,27 @@
 #include "holberton.h"
 
 /**
- * _strstr -
- *
- * Return: 
+ * _strstr - locates a substring
+ * @s: string to search in
+ * @f: substring to search for
+ * Return: pointer to the beginning of the located substring,
+ * or NULL if the substring is not found
  */
-char *_strstr(char *haystack, char *needle)
+char *_strstr(char *s, char *f)
 {
+	int j;
 
+	for (j = 0; *s; s++)
+	{
+		if (*s != *f)
+			f = f - j, j = 0;
+		else
+			j++, f++;
+		if (!*f)
+		{
+			s = s - j + 1;
+			break;
+		}
+	}
+	return (s);
 }
