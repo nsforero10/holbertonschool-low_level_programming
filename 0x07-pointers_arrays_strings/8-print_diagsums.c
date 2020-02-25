e597daf643438c4d6e5ab8316408eb7eccd7fad3
Nicolas Forero <ns.forero10@gmail.com>
2020-02-25 03:50:50 +0000

    [Fix] Fixing space on 8 task

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,5 +15,5 @@ void print_diagsums(int *a, int size)
 		sum1 += a[i];
 	for (i = size - 1; i < size * size - 1; i += size - 1)
 		sum2 += a[i];
-	printf("%d,%d\n", sum1, sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
