544f1b9b6ff7480118fb81fe2c565871c36b76bc
Nicolas Forero <ns.forero10@gmail.com>
2020-02-26 20:54:42 +0000

    [Add] Adding 0x08 3 task

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,11 +1,15 @@
 #include "holberton.h"
 
 /**
- * factorial -
- *
- * Return: 
+ * factorial - calculates the facotrial of a number
+ * @n: number to calculate the factorial
+ * Return: the factorial of the number
  */
 int factorial(int n)
 {
-
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
 }
