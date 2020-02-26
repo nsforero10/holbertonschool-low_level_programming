a36c6ae3d843407400be58db1c46779520970e8b
Nicolas Forero <ns.forero10@gmail.com>
2020-02-26 23:08:44 +0000

    [Add] Adding 0x08 5 task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,30 @@
 #include "holberton.h"
 
 /**
- * _sqrt_recursion -
- *
- * Return: 
+ * calculate - returns the natural square root of a number.
+ * @x: num to check if is sqrt
+ * @n: nom to search for it sqrt
+ * Return: square root
+ */
+
+int calculate(int x, int n)
+{
+	if (x == n)
+		return (-1);
+	if (x * x == n)
+		return (x);
+	x++;
+	aux(x, n);
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
+ * @n: num to seaarch for it square root
+ * Return: square root
  */
 int _sqrt_recursion(int n)
 {
-
+	if (n == 1)
+		return (1);
+	return (aux(2, n));
 }
