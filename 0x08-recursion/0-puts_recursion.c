54ba984e3d4bf9979667be1d8c4af55fbbeb19cf
Nicolas Forero <ns.forero10@gmail.com>
2020-02-26 20:10:08 +0000

    [Fix] Fixing 0x08 1 task

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,12 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(s[0]);
-	s++;
-	if (s[0] == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
+	_putchar(*s);
 	_puts_recursion(s++);
 }
