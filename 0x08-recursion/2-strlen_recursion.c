ff6ade9cef66c85ad9b2bf257703d7aeb3ffcede
Nicolas Forero <ns.forero10@gmail.com>
2020-02-26 20:28:51 +0000

    [Add] Adding 0x08 2 task

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,11 +1,14 @@
 #include "holberton.h"
 
 /**
- * _strlen_recursion -
- *
- * Return: 
+ * _strlen_recursion - calculate lenght of a string
+ * @s: string to be messured
+ * Return: the lenght of the string
  */
 int _strlen_recursion(char *s)
 {
-
+	if (!*s)
+		return (0);
+	s++;
+	return (1 + _strlen_recursion(s));
 }
