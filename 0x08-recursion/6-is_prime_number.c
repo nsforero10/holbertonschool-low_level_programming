784b26115ea39aacd1b3a39ef1f1570452ec1e64
Nicolas Forero <ns.forero10@gmail.com>
2020-02-27 00:31:54 +0000

    [Add] Adding 0x08 6 task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,11 +1,29 @@
 #include "holberton.h"
 
 /**
- * is_prime_number -
- *
- * Return: 
+ * srchprime - search for a prime
+ * @x: num to check if is multiple
+ * @n: num to check if is prime
+ * Return: 0 if n is not prime, 1 if is prime
+ */
+int srchprime(int x, int n)
+{
+	if ((n % x) == 0)
+		return (0);
+	if (x >= (n / 2))
+		return (1);
+	x++;
+	return (srchprime(x, n));
+}
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number,
+ * otherwise return 0.
+ * @n: the num to check if is prime
+ * Return: 0 if n is not prime, 1 if is prime
  */
 int is_prime_number(int n)
 {
-
+	if (n == 1 || n == -1)
+		return (0);
+	return (srchprime(2, n));
 }
