b0f9d68bb07a200e755449da3a951dc2e604e5c2
Nicolas Forero <ns.forero10@gmail.com>
2020-02-27 04:52:40 +0000

    [Feature] Adding 0x08 7 task

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,11 +1,38 @@
 #include "holberton.h"
 
 /**
- * is_palindrome -
- *
- * Return: 
+ * size - calculate the size
+ * @s: sstring to messure
+ * Return: size of the string
  */
+int size(char *s)
+{
+	return ((!*s) ? (0) : (s++, (1 + size(s))));
+}
+
+/**
+ * compare - calculate the size
+ * @p: strig to check
+ * @size: size of the string
+ * Return: 1 if is palindrome 0 if not
+ */
+
+int compare(char *p, int size)
+{
+	if (*p != *(p + (--size)))
+		return (0);
+	p++, size--;
+	return (1 * compare(p, size));
+}
+
+
+/**
+ * is_palindrome - check if a string is palindrome
+ * @s: string to be checked
+ * Return: 1 if is palindrome 0 if not
+ */
+
 int is_palindrome(char *s)
 {
-
+	return (compare(s, size(s)));
 }
