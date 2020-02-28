60a0b677a43a1d6570820afcb8f202c179178f6d
nsforero10 <49564133+nsforero10@users.noreply.github.com>
2020-02-28 00:00:19 -0500

    Create 101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * wildcmp - checks 
+ * @s1: string 1 to compare
+ * @s2: string 2 to compare
+ * Return: 1 if is equivalent 0 if not
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			return (1);
+	}
+	return (0);
+}
