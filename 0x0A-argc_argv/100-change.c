ad49ff267b836aedec8b9d2f1502db67e7e67772
Nico F <ns.forero10@gmail.com>
2020-03-01 00:18:19 +0000

    [Feature] Adding 0x0A 100

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - prints the number of arguments of the program
+ * @argc: number of arguments
+ * @argv: pointer to the list of strings passsed as arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int num_coins, res, i;
+	int coins[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	res = atoi(argv[1]);
+	if (res <= 0)
+	{
+		printf("0\n");
+		return (1);
+	}
+	for (i = 0; res != 0; i++)
+	{
+		num_coins += res / coins[i];
+		res = res % coins[i];
+	}
+	printf("%d\n", num_coins);
+	return (1);
+}
