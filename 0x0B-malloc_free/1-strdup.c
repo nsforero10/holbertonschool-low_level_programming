85e00257339e3d1e2db7c95a6b2f9798781ba6b2
Nico Forero <ns.forero10@gmail.com>
2020-03-03 17:19:52 -0500

    [Feature] Addint 0x0B 1 task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ *_strlength - calulate the length of a string
+ *@p: string to messure
+ *Return: length of the string
+ */
+unsigned int _strlength(char *p)
+{
+	return ((!*p) ? (0) : (1 + (_strlength(++p))));
+}
+
+/**
+ * _strdup - create an array with the an specified number
+ * @str: string to copy
+ * Return: 0 if something fails or size is 0, and a pointer to the copy
+ */
+char *_strdup(char *str)
+{
+	char *scp;
+	unsigned int size, i;
+
+	if (!str)
+		return (NULL);
+	size = _strlength(str);
+	scp = malloc(size * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		scp[i] = str[i];
+		i++;
+	}
+	return (scp);
+}
