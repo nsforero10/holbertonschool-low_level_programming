777e8ab5a522e08751529e8d3199e8b38493b2b7
Nico Forero <ns.forero10@gmail.com>
2020-03-05 09:57:25 -0800

    [Feature] Adding tasks 1,2 0x0C

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,11 +1,48 @@
 #include "holberton.h"
-
+#include <stdlib.h>
 /**
- * string_nconcat -
- *
- * Return: 
+ * string_nconcat - concatenates the first string
+ * with the frist n chars of the second
+ * @s1: the first string to concatenate
+ * @s2: the second string to concatenate
+ * @n: the size that want to be used on s2
+ * Return: pointer to the concatened string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	char *ptr = NULL;
+	unsigned int cnt = 0, aux = 0, i = 0;
 
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	while (*s1)
+		cnt++, s1++, aux++;
+	s1 -= aux;
+	aux = 0;
+	while (*s2 && aux < n)
+		cnt++, s2++, aux++;
+	s2 -= aux;
+	ptr = malloc(cnt * sizeof(char));
+	if (!ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	i = 0;
+	while (*s1)
+	{
+		*ptr = *s1;
+		s1++, ptr++, i++;
+	}
+	aux = 0;
+	while (*s2 && aux < n)
+	{
+		*ptr = *s2;
+		s2++, ptr++, i++, aux++;
+	}
+	*ptr = '\0';
+	ptr -= i;
+	return (ptr);
 }
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,11 +1,25 @@
 #include "holberton.h"
 
 /**
- * _calloc -
- *
- * Return: 
+ * _calloc - allocate nmemb of size
+ * @nmemb: number of elements
+ * @size: size of the type
+ * Return: the pointer to the memory reserved
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *ptr = NULL;
+	unsigned int i;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (!ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	for (i = 0; i < size * nmemb; i++)
+		*((char *)ptr + i) = 0;
+	return (ptr);
 }
