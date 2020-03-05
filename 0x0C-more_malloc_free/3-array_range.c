58d009c38b095804042a9c626836113dccef1b12
Nico Forero <ns.forero10@gmail.com>
2020-03-05 13:15:16 -0800

    [Feature] Adding 0x0C 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,11 +1,28 @@
 #include "holberton.h"
 
 /**
- * array_range -
- *
- * Return: 
+ * array_range - allocs on mem from min to max
+ * @min: min int
+ * @max: max int
+ * Return: return pointer to min
  */
 int *array_range(int min, int max)
 {
+	int *ptr, i;
 
+	if (min > max)
+		return (NULL);
+	ptr = malloc(sizeof(int) * (max - min + 1));
+	if (!ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	i = 0;
+	while (i <= (max - min + 1))
+	{
+		ptr[i] = (min + i);
+		i++;
+	}
+	return (ptr);
 }
