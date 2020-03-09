a8cb43136872d6dafea9e49c421edb08ecc01d1d
Nico Forero <ns.forero10@gmail.com>
2020-03-09 12:48:01 -0700

    [Feature] Adding 0x0E 0 task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,26 @@
+#include "dog.h"
+
+/**
+ *struct dog -dog
+ * @name : char*
+ * @age: int
+ * @owner char*
+
+ *Description: define a dog structure
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * free_dog -
+ *
+ * Return:
+ */
+void free_dog(dog_t *d)
+{
+
+}
