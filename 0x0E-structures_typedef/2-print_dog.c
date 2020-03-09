3e5f08424bb7b2e660be850689be5d2b01662719
Nico Forero <ns.forero10@gmail.com>
2020-03-09 13:52:29 -0700

    [Fix #4] Fixing 0x0E NULL 2 task

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
+
 /**
  * print_dog - print a dog atributes
  * @d: dog
@@ -9,12 +10,12 @@ void print_dog(struct dog *d)
 {
 	if (!d)
 		return;
-	if (!(d->name))
+	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
-	if (!(d->owner))
+	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->name);
