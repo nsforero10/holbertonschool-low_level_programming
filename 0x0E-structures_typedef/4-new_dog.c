01838766eed1a00157583eb1bcde98c3077a27e9
Nico Forero <ns.forero10@gmail.com>
2020-03-09 16:44:38 -0700

    [Fix] Fixing 0x0E 4 segmentation fault

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,60 +8,51 @@
  */
 int _strlen(char *p)
 {
-	return (!*p) ? (0) : (1 + _strlen(++p));
+	return ((!*p) ? (1) : (1 + _strlen(++p)));
 }
 
 
 /**
- * print_dog - print a dog atributes
- * @d: dog
- * Return: void
+ * new_dog - create a new dog
+ * @name: new dog's name
+ * @age: new dog's age
+ * @owner: new dog's owner
+ * Return: the new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
-	char *cpname;
-	char *cpowner;
-	int i, szname = _strlen(name);
+	char *cpname, *cpowner;
+	int i;
 
-	printf("%d\n",szname);
-
-	cpname = malloc(szname + 1);
-	printf("after cpname");
+	cpname = malloc(_strlen(name));
 	if (!cpname)
-	{
-		free(cpname);
 		return (NULL);
-	}
 	for (i = 0; *name; i++)
 	{
 		*cpname = *name;
 		cpname++, name++;
 	}
-	cpname = '\0';
+	*cpname = '\0';
 	cpname -= i;
-	name -= i;
-	cpowner = malloc(_strlen(owner) + 1);
+	cpowner = malloc(_strlen(owner));
 	if (!cpowner)
 	{
 		free(cpname);
-		free(cpowner);
 		return (NULL);
 	}
-	for (i = 0; *owner ;i++)
+	for (i = 0; *owner; i++)
 	{
 		*cpowner = *owner;
 		cpowner++, owner++;
 	}
-	cpowner = '\0';
+	*cpowner = '\0';
 	cpowner -= i;
-	owner -= i;
 	dog = malloc(sizeof(dog_t));
 	if (!dog)
 	{
 		free(owner);
 		free(name);
-		free(dog);
 		return (NULL);
 	}
 	dog->name = cpname;
