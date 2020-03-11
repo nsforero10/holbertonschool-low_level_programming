1267f2d63bfeae51ebfbf2978b707a00ae2309bf
Nico Forero <ns.forero10@gmail.com>
2020-03-11 12:44:17 -0500

    [Feature] Adding task 0,1,2 0x0F

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer.
+ * @array: array where the integer will be searched
+ * @size: the size of the array
+ * @cmp: function callback to compare
+ * Return: the index of the int when te callback returns 1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two integers
+ * @a: frist integer
+ * @b: second integer
+ * Return: Return the sum of the two integers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_add - substract two integers
+ * @a: frist integer
+ * @b: second integer
+ * Return: Return the difference of the two integers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_add - multiply two integers
+ * @a: frist integer
+ * @b: second integer
+ * Return: Return the product of the two integers
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_add - dived frist integer into the second integer
+ * @a: frist integer
+ * @b: second integer
+ * Return: Return the result of the division betweeen two integers
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_add - modules first integer into the second integers
+ * @a: frist integer
+ * @b: second integer
+ * Return: Return the reminder of the division between a into b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
