09027ee71f9a424685015a46099a6c130a84ee43
Nico Forero <ns.forero10@gmail.com>
2020-03-13 08:48:06 -0500

    [Feature] Adding 1 task 0x10

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,11 +1,26 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
- * print_numbers -
- *
- * Return: 
+ * print_numbers -print numbers s
+ * @separator: separator to be between
+ * @n: the number of args
+ * Return: nothing
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	va_list args;
+	unsigned int i = 0;
 
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i < n - 1 && *separator)
+			printf("%d%s", va_arg(args, int), separator);
+		else
+			printf("%d", va_arg(args, int));
+	}
+	va_end(args);
+	printf("\n");
 }
