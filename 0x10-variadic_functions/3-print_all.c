8d301e923710dad867b1dc038dbddbbce2560377
Nico Forero <ns.forero10@gmail.com>
2020-03-13 13:37:08 -0500

    [Feature] Adding task 0x10 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,11 +1,88 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
 
 /**
- * print_all -
- *
- * Return: 
+ * char_funct - gets the function to printchar
+ * @c: char to print
+ * Return: nothing
+ */
+void char_funct(va_list c)
+{
+	printf("%c", va_arg(c, int));
+}
+
+/**
+ * char_funct - gets the function to printchar
+ * @c: char to print
+ * Return: nothing
+ */
+void int_funct(va_list c)
+{
+	printf("%d", va_arg(c, int));
+}
+
+/**
+ * char_funct - gets the function to printchar
+ * @c: char to print                                                                      
+ * Return: nothing
+ */
+void float_funct(va_list c)
+{
+	printf("%f", va_arg(c, double));
+}
+
+/**
+ * char_funct - gets the function to printchar
+ * @c: char to print
+ * Return: nothing
+ */
+void string_funct(va_list c)
+{
+	printf("%s", va_arg(c, char *));
+}
+/**
+ * get_funct - gets the function to use
+ * @c: char to check
+ * Return: a pointer to the function
+ */
+void (*get_funct(char c))(va_list)
+{
+	obj_t objs[] = {
+		{'c', char_funct},
+		{'i', int_funct},
+		{'f', float_funct},
+		{'s', string_funct},
+		{'\0', NULL}
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if((objs[i].chr) == c)
+			return (objs[i].f);
+		i++;
+	}
+	return (NULL);
+}
+/**
+ * print_all - print anythings
+ * @format: the format to print
+ * Return: nothing
  */
 void print_all(const char * const format, ...)
 {
+	va_list args;
+	unsigned int i = 0;
 
+	printf("Hola");
+	va_start(args, format);
+	while (format[i])
+	{
+		get_funct(format[i])(args);
+		i++;
+	}
+	va_end(args);
+	printf("\n");
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,17 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct obj -Structo obj
+ * @chr: character
+ * @f: function match
+ */
+typedef struct obj{
+	char chr;
+	void (*f)(va_list);
+} obj_t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
