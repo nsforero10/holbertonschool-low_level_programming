41e0e5f8bb4b75dffb9fb6c78e94a731e4f61328
Nico Forero <ns.forero10@gmail.com>
2020-03-14 12:36:19 -0500

    [Fix] Fixing 0x10 2 separator case

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%s", va_arg(args, char *));
-		if (i < n - 1 && *separator)
+		if (i < n - 1 && separator)
 			printf("%s", separator);
 	}
 	va_end(args);
