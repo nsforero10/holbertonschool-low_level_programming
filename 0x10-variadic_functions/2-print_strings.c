ffe5097c8c61dd90c0be47f9aaf3aad20263e880
Nico Forero <ns.forero10@gmail.com>
2020-03-14 12:27:40 -0500

    [Fix] Fixing 0x10 2 betty

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,9 +2,10 @@
 #include <stdio.h>
 #include <stdarg.h>
 /**
- * print_strings -
- *
- * Return:
+ * print_strings - prints strings separated b a separator
+ * @separator: the char o string to separate the strings
+ * @n: the number of arguments
+ * Return: nothing
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
