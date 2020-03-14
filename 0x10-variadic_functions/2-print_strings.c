4f7dda4cf4ef34b101a4985530b9c959a7d565cb
Nico Forero <ns.forero10@gmail.com>
2020-03-14 12:41:00 -0500

    [Fix] Fixing 0x10 2 null case

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,11 +11,16 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i = 0;
+	char *ptr;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(args, char *));
+		ptr = va_arg(args, char *);
+		if (ptr)
+			printf("%s", ptr);
+		else
+			printf("(nil)");
 		if (i < n - 1 && separator)
 			printf("%s", separator);
 	}
