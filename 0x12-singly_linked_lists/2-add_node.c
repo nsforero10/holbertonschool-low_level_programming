749e99468214c4b3bec7149dbc0249b7bdc51a78
Nico Forero <ns.forero10@gmail.com>
2020-03-19 16:30:11 -0500

    [Feature] Adding task 2 0x12

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,11 +1,38 @@
 #include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
 
 /**
- * add_node -
- *
- * Return: 
+ * _strlen - messures a string
+ * @ptr: string to messure
+ * Return: the size of the string
+ */
+unsigned int _strlen(char *ptr)
+{
+	unsigned int i = 0;
+
+	while (*ptr)
+		i++, ptr++;
+	return (i);
+}
+
+/**
+ * add_node - a new node on the top of the
+ * @head: the head of the linked list
+ * @str: the string for the new node
+ * Return: a pointer to the new node
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	char *ptr = strdup(str);
+	list_t *new = malloc(sizeof(list_t));
 
+	if (!new)
+		return (NULL);
+	(*new).str = ptr;
+	(*new).len = _strlen(ptr);
+	(*new).next = *head;
+	*head = new;
+	return (*head);
 }
