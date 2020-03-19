305cf2cfc868f791e5b0860066eee1442ed98627
Nico Forero <ns.forero10@gmail.com>
2020-03-19 15:17:21 -0500

    [Feature] Adding task 1 0x12

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,15 +1,15 @@
 #include "lists.h"
 
 /**
- * list_len -
- *
- * Return: 
+ * list_len - messures a linked list
+ * @h: head of linked list
+ * Return: the number of nodes of the linked list
  */
 size_t list_len(const list_t *h)
 {
 	size_t size = 0;
-	
+
 	while (h)
-		h = h->next, size++; 
+		h = h->next, size++;
 	return (size);
 }
