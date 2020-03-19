772b89dea1367887f64f8eecfa50a1358cae30a3
Nico Forero <ns.forero10@gmail.com>
2020-03-19 18:04:42 -0500

    [Feature] Adding task 3 0x12

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,11 +1,38 @@
 #include "lists.h"
-
+#include <string.h>
+#include <stdlib.h>
 /**
- * add_node_end -
- *
- * Return: 
+ * add_node_end - add a new node
+ * @head: the head of the linked list
+ * @str: the new string
+ * Return: the head
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
+	char *ptr = strdup(str);
+	unsigned int i = 0;
+	list_t *new = malloc(sizeof(list_t)), *last = NULL;
 
+	if (!new)
+		return (NULL);
+
+
+	while (*str)
+	{
+		i++, str++;
+	}
+	new->str = ptr;
+	new->len = i;
+	new->next = NULL;
+
+	last = *head;
+	if (!last)
+		*head = new;
+	else
+	{
+		while (last->next)
+			last = last->next;
+		last->next = new;
+	}
+	return (*head);
 }
