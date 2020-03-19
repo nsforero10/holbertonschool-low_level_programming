0d62807768bede2f9198e6c0e6416067bed1ea6d
Nico Forero <ns.forero10@gmail.com>
2020-03-19 18:48:58 -0500

    [Feature] Adding task 5 0x12

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void printBeforName(void) __attribute__ ((constructor));
+/**
+ * printBeforName - prints something bofore main be executed
+ * Return: nothing
+ */
+void printBeforName(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
