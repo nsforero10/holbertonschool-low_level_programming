4099f986e9a91f6720da5b7a43882e2f177be174
Nico Forero <ns.forero10@gmail.com>
2020-03-19 18:27:27 -0500

    [Feature] Adding task 4 0x12

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,11 +1,19 @@
 #include "lists.h"
 
 /**
- * free_list -
- *
- * Return: 
+ * free_list - frees a linked list
+ * @head: the head of the string
+ * Return: nothing
  */
 void free_list(list_t *head)
 {
+	list_t *temp;
 
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
 }
