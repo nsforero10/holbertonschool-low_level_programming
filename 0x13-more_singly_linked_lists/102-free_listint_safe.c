0fcf58337720c76d8990d67d285a5a1c5a8a69e0
Nico Forero <ns.forero10@gmail.com>
2020-03-24 21:33:39 -0500

    [Feature] Adding task 102 0x13

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,11 +1,29 @@
 #include "lists.h"
 
 /**
- * free_listint_safe -
- *
- * Return: 
+ * free_listint_safe - frees a linked list
+ * @h: the head of the linked list
+ * Return: the size of the list
  */
 size_t free_listint_safe(listint_t **h)
 {
+	listint_t *temp, *prev;
+	unsigned int size = 0;
 
+	temp = *h;
+	if (temp == NULL || h == NULL)
+		return (0);
+
+	while (temp != NULL)
+	{
+		prev = temp;
+		temp = temp->next;
+
+		free(prev);
+		size++;
+		if (temp >= prev)
+			break;
+	}
+	*h = 0;
+	return (size);
 }
