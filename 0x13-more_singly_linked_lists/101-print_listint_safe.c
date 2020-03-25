e4534718e27b01578974aef2651525ada2b28fec
Nico Forero <ns.forero10@gmail.com>
2020-03-24 19:32:18 -0500

    [Fix #1] Fixing betty list.h 0x13

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,5 +7,14 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
+	size_t size = 0;
+	const listint_t *temp = NULL;
 
+	temp = head;
+	while (temp)
+	{
+		printf("[%p] %d\n", (void *)temp, temp->n);
+		temp = temp->next, size++;
+	}
+	return (size);
 }
