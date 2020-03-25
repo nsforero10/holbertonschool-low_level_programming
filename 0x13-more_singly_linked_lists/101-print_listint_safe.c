f1d730968d654ba3fd2f2043c4e97badd002c92f
Nico Forero <ns.forero10@gmail.com>
2020-03-24 21:30:13 -0500

    [Feature] Adding task 101 0x13

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,20 +1,28 @@
 #include "lists.h"
 
 /**
- * print_listint_safe -
- *
- * Return: 
+ * print_listint_safe - print a linked list
+ * @head: the head of the linked list
+ * Return: the size of the linked list
  */
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t size = 0;
-	const listint_t *temp = NULL;
+	const listint_t *temp = NULL, *back;
 
+	if (!head)
+		return (0);
 	temp = head;
 	while (temp)
 	{
+		back = temp;
 		printf("[%p] %d\n", (void *)temp, temp->n);
 		temp = temp->next, size++;
+		if (temp >= back)
+		{
+			printf("-> [%p] %d\n", (void *)temp, temp->n);
+			break;
+		}
 	}
 	return (size);
 }
