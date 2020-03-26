e44f4916287bf17e34f0f71e832d86d611ae872c
Nico Forero <ns.forero10@gmail.com>
2020-03-26 17:58:40 -0500

    [Fix] Fixing betty 1 task 0x14

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,28 +1,28 @@
 #include "holberton.h"
 
 /**
- * dec_bin - print in binary with bitwise operator
- * @n: decimal number
- * Return: void - just prints
+ * print_int_bin - prints a integer number as binary
+ * @n: number to print
+ * Return: nothing
  */
-void dec_bin(unsigned long int n)
+void print_int_bin(unsigned long int n)
 {
 	if (n == 0)
 		return;
 
-	dec_bin(n >> 1);
+	print_int_bin(n >> 1);
 	n = n & 1;
 	_putchar(n + 48);
 }
 /**
- * print_binary - print decimal in binary
- * @n: decimal number
- * Return: void - just prints
+ * print_binary - prints binary representation int
+ * @n: number to print
+ * Return: nothing
  */
 void print_binary(unsigned long int n)
 {
 	if (n == 0)
 		_putchar('0');
 	else
-		dec_bin(n);
+		print_int_bin(n);
 }
