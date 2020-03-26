c1ff4abee4ce67dd2225a33c93287b500bacb857
Nico Forero <ns.forero10@gmail.com>
2020-03-26 16:59:05 -0500

    [Feature] Adding 1 task 0x14

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,11 +1,23 @@
 #include "holberton.h"
 
 /**
- * print_binary -
- *
- * Return: 
+ * print_binary - print binary representation of a int
+ * @n: the integer top prpint
+ * Return: nothing
  */
 void print_binary(unsigned long int n)
 {
-
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	else
+	{
+		while (n != 0)
+		{
+			_putchar((n & 1) ? '1' : '0');
+			n = n >> 1;
+		}
+	}
 }
