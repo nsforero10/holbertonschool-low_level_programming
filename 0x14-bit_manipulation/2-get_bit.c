53fb5b6035b5fa73f15286f500ac84d04b925b98
Nico Forero <ns.forero10@gmail.com>
2020-03-26 18:08:45 -0500

    [Fix] Fixing betty 1 task 0x14

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,11 +1,17 @@
 #include "holberton.h"
 
 /**
- * get_bit -
- *
- * Return: 
+ * get_bit - gets a bit in a specified index
+ * @n: the int to search the bin
+ * @index: the index to search for
+ * Return: the bit in the given index
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-
+	while (index)
+	{
+		n = n >> 1;
+		index--;
+	}
+	return (n)
 }
