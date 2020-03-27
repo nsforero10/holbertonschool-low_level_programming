2f7a502066d0ae7398c98f3bc12d564175e55bf3
Nico Forero <ns.forero10@gmail.com>
2020-03-26 20:51:01 -0500

    [Feature] Adding 100 task 0x14

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,11 +1,15 @@
 #include "holberton.h"
 
 /**
- * get_endianness -
- *
- * Return: 
+ * get_endianness - checks the edianness
+ * Return: 1 if little, 0 if big
  */
 int get_endianness(void)
 {
+	int n = 1;
+	char *p = (char *)&n;
 
+	if (*p)
+		return (1);
+	return (0);
 }
