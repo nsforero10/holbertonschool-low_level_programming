6925be6215cdd74c974b4502f35049d7dca1875d
Nico Forero <ns.forero10@gmail.com>
2020-03-26 20:21:38 -0500

    [Feature] Adding 4 task 0x14

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,11 +1,15 @@
 #include "holberton.h"
 
 /**
- * clear_bit -
- *
- * Return: 
+ * clear_bit - clears a bit in a int
+ * @n: pointer to the int to clear
+ * @index: index to clear
+ * Return: 1 if success, -1 if fails
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-
+	if (index > 63)
+		return (-1);
+	*n = *n & ~(1 << index);
+	return (1);
 }
