2fa44eb9a831f03d0ccc3b9d3ea4dff98f29de9d
Nico Forero <ns.forero10@gmail.com>
2020-03-26 20:08:43 -0500

    [Feature] Adding 3 task 0x14

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,11 +1,15 @@
 #include "holberton.h"
 
 /**
- * set_bit -
- *
- * Return: 
+ * set_bit - sets a bit ina specific index
+ * @n: pointer to the int to set the value
+ * @index: the index to insert the one
+ * Return: 1 if success, -1 y fails
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-
+	if (index > 63)
+		return (-1);
+	*n = *n | (1 << index);
+	return (1);
 }
