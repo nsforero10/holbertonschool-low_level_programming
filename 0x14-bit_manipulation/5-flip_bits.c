5b38f990ea14b3ab51aa585a7f1500c0eb426f52
Nico Forero <ns.forero10@gmail.com>
2020-03-26 20:45:10 -0500

    [Feature] Adding 5 task 0x14

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,11 +1,18 @@
 #include "holberton.h"
 
 /**
- * flip_bits -
- *
- * Return: 
+ * flip_bits -calculastes how many bits are needed to flip a int in another
+ * @n: first int
+ * @m:  second int
+ * Return: the number of bits needed
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned long int nbits = 0;
+	int bchange = 0;
 
+	nbits = n ^ m;
+	while(nbits)
+		bchange += nbits & 1, nbits >>= 1;
+	return (bchange);
 }
