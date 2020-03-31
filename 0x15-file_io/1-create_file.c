ee1242d6010145f89e5eb9d0437d8e39a616060b
Nico Forero <ns.forero10@gmail.com>
2020-03-31 15:04:12 -0500

    [Feature] Adding 1 task 0x14

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,36 @@
 #include "holberton.h"
 
 /**
- * create_file -
- *
- * Return: 
+ * _strlen - messures a file
+ * @p: the pointer
+ * Return: the size
+ */
+size_t _strlen(char *p)
+{
+	size_t i = 0;
+
+	while (*p)
+		i++, p++;
+	return (i);
+}
+
+/**
+ * create_file - creates a file.
+ * @filename: the file name
+ * @text_content: the file content
+ * Return: the size
  */
 int create_file(const char *filename, char *text_content)
 {
+	int fdo, fdw;
 
+	if (!filename || !text_content)
+		return (-1);
+	fdo = open(filename, O_CREAT | O_RDWR, 0600);
+	if (fdo == -1)
+		return (-1);
+	fdw = write(fdo, text_content, _strlen(text_content));
+	if (fdw == -1)
+		return (-1);
+	return (1);
 }
