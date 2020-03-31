65d7f8539c7c3bd9f01c1bc49cf7b06fac47f46a
Nico Forero <ns.forero10@gmail.com>
2020-03-31 15:16:31 -0500

    [Fix] Fixing 1 text_content 0x14

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,14 +24,17 @@ int create_file(const char *filename, char *text_content)
 {
 	int fdo, fdw;
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
-	fdo = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fdo = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (fdo == -1)
 		return (-1);
-	fdw = write(fdo, text_content, _strlen(text_content));
-	if (fdw == -1)
-		return (-1);
+	if (text_content)
+	{
+		fdw = write(fdo, text_content, _strlen(text_content));
+		if (fdw == -1)
+			return (-1);
+	}
 	close(fdo);
 	return (1);
 }
