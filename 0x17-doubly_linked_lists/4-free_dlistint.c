8cad57c3cdb8aa389a882efede02282570451482
Nicolas Forero <ns.forero10@gmail.com>
2020-05-01 21:23:44 -0500

    [Feature] Adding 4 task 0x17

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,11 +1,18 @@
 #include "lists.h"
 
 /**
- * free_dlistint -
- *
- * Return: 
+ * free_dlistint - frees a dlistint_t list.
+ * @head of the new linked list
+ * Return: nothing
  */
 void free_dlistint(dlistint_t *head)
 {
+	dlistint_t *crnt = head, *tmp = NULL;
 
+	while (crnt)
+	{
+		tmp = crnt;
+		crnt = crnt->next;
+		free(tmp);
+	}
 }
