d74472552908d9dab9f88021a6802ae315443543
Nicolas Forero <ns.forero10@gmail.com>
2020-05-01 20:28:07 -0500

    [Feature] Adding 1 task 0x17

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,18 @@
 #include "lists.h"
 
 /**
- * dlistint_len -
- *
- * Return: 
+ * dlistint_len - messures a linked list
+ * @h: the head of the linked list 
+ * Return: the size of the linked list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
+    	const dlistint_t *crnt = h;
+	size_t len = 0;
 
+	while (crnt)
+	{
+		crnt = crnt->next, len++;
+	}
+	return (len);
 }
