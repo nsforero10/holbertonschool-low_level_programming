39c607e09e4567ae07a6472c942b4fa02c4c082f
Nicolas Forero <ns.forero10@gmail.com>
2020-05-01 21:32:51 -0500

    [Feature] Adding 5 task 0x17

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,11 +1,20 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index -
- *
- * Return: 
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * @head: the ehead of the linked list
+ * @index: the index of the node to return
+ * Return: the nth node of a dlistint_t linked list, if the node does not exist, return NULL
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	dlistint_t *crnt = head;
 
+	while (index > 0)
+	{
+		crnt = crnt->next, index--;
+		if (!crnt)
+			return(NULL);
+	}
+	return (crnt);
 }
