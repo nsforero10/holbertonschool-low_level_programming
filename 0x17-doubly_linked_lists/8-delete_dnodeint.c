79811e00d48c77296473da47f26da777e5d57d01
Nicolas Forero <ns.forero10@gmail.com>
2020-05-02 00:15:20 -0500

    [Feature] Adding 8 task 0x17

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,11 +1,37 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index -
- *
- * Return: 
+ * delete_dnodeint_at_index - deletes the node
+ * at index index of a dlistint_t linked list.
+ * @head: head of the linked list
+ * @index: index of the node to delete
+ * Return: 1 if it succeeded, -1 if it failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	dlistint_t *crnt = *head;
 
+	if (!*head)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = crnt->next;
+		if (crnt->next)
+			crnt->next->prev = crnt->prev;
+	}
+	else
+	{
+		while (index > 0)
+		{
+			crnt = crnt->next, index--;
+			if (!crnt)
+				return (-1);
+		}
+		if (crnt->prev)
+			crnt->prev->next = crnt->next;
+		if (crnt->next)
+			crnt->next->prev = crnt->prev;
+	}
+	return (1);
 }
