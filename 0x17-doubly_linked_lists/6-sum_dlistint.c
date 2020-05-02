bcd203cadcc183edb02e6a79e20288eef647708b
Nicolas Forero <ns.forero10@gmail.com>
2020-05-01 22:00:35 -0500

    [Feature] Adding 6 task 0x17

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,11 +1,16 @@
 #include "lists.h"
 
 /**
- * sum_dlistint -
- *
- * Return: 
+ * sum_dlistint - sum of all the data (n) of a dlistint_t linked list.
+ * @head: head of the linked list
+ * Return: the sum, 0 if the list is empty
  */
 int sum_dlistint(dlistint_t *head)
 {
+	int sum = 0;
+	dlistint_t *crnt = head;
 
+	while (crnt)
+		sum += crnt->n, crnt = crnt->next;
+	return (sum);
 }
