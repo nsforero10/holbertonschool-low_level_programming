3cdb883ccd912c0eff9bd7069ce9778b3f0a63eb
Nicolas Forero <ns.forero10@gmail.com>
2020-05-01 20:47:31 -0500

    [Feature] Adding 2 task 0x17

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,11 +1,24 @@
 #include "lists.h"
 
 /**
- * add_dnodeint -
- *
- * Return: 
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list.
+ * @head: the head of the linked list
+ * @n: the number the new node
+ * Return: the address of the new element, or NULL if it failed
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	dlistint_t *new = malloc(sizeof(dlistint_t));
 
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	new->next =  *head;
+
+	if(*head)
+		(*head)->prev = new;
+	*head = new;
+	return (new);
 }
