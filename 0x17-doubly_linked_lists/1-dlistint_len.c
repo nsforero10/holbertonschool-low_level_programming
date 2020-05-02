1556822c1bf1c021fb4455c52d32ab37d620fe83
Nicolas Forero <ns.forero10@gmail.com>
2020-05-01 20:30:07 -0500

    [Fix] Fixing Betty 1 task 0x17

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,12 +2,12 @@
 
 /**
  * dlistint_len - messures a linked list
- * @h: the head of the linked list 
+ * @h: the head of the linked list
  * Return: the size of the linked list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    	const dlistint_t *crnt = h;
+	const dlistint_t *crnt = h;
 	size_t len = 0;
 
 	while (crnt)
