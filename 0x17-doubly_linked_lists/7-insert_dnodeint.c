d28ad5c6c1cac5e1a6d5d308fdb43b22eb5d7c55
Nicolas Forero <ns.forero10@gmail.com>
2020-05-02 00:26:55 -0500

    [Fix] Fixing 7 task 0x17

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,19 +11,21 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new = NULL, *crnt = *h;
 
-	while (idx > 1)
+	while (idx > 0)
 	{
 		crnt = crnt->next, idx--;
 		if (!crnt)
 			return (NULL);
 	}
+
 	new = malloc(sizeof(dlistint_t));
 	if (!new)
 		return (NULL);
 	new->n = n;
-	new->next = crnt->next;
-	new->prev = crnt;
-	crnt->next = new;
+	new->next = crnt;
+	if (crnt->prev)
+		new->prev = crnt->prev;
+	crnt->prev = new;
 	if (new->next)
 		new->next->prev = new;
 	return (new);
