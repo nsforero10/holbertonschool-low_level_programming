865465c6092b4b70fca0fad582a2e3cf7f06ff3f
Nicolas Forero <ns.forero10@gmail.com>
2020-05-02 19:16:25 -0500

    [Fix] Fixing 7 task 0x17

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,23 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - messures a linked list
+ * @h: the head of the linked list
+ * Return: the size of the linked list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	const dlistint_t *crnt = h;
+	size_t len = 0;
+
+	while (crnt)
+	{
+		crnt = crnt->next, len++;
+	}
+	return (len);
+}
+
+/**
  * insert_dnodeint_at_index - inserts a new node at a given position.
  * @h: head of the linked list
  * @idx: the index to insert the new node
@@ -9,37 +26,44 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new = NULL, *crnt = *h;
+	dlistint_t *new = malloc(sizeof(dlistint_t)), *crnt = *h;
+	unsigned int i = 0;
 
+	if (!new)
+		return (NULL);
+	new->n = n;
 	if (idx == 0)
 	{
-		new = malloc(sizeof(dlistint_t));
-		if (!new)
-			return (NULL);
-		new->n = n;
-		new->next = crnt;
-		new->prev = NULL;
-		crnt->next->prev = new;
-		*h = new;
+		if (!*h)
+			new->next = NULL, new->prev = NULL, *h = new;
+		else
+		{
+			new->prev = NULL;
+			new->next = *h;
+			(*h)->prev = new;
+			*h = new;
+		}
+	}
+	else if (idx == dlistint_len(*h))
+	{
+		while (crnt->next)
+			crnt = crnt->next;
+		new->next = NULL;
+		crnt->next = new;
+		new->prev = crnt;
 	}
 	else
 	{
-		while (idx > 0)
+		while (i < idx)
 		{
-			crnt = crnt->next, idx--;
-			if (!crnt)
+			if (!crnt->next)
 				return (NULL);
+			crnt = crnt->next, i++;
 		}
-
-		new = malloc(sizeof(dlistint_t));
-		if (!new)
-			return (NULL);
-		new->n = n;
 		new->next = crnt;
 		new->prev = crnt->prev;
 		crnt->prev->next = new;
 		crnt->prev = new;
-
 	}
 	return (new);
 }
