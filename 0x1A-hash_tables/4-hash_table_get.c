d5bb67baa04fe886a6d1bb931a459cb30ba18397
Nicolas Forero <ns.forero10@gmail.com>
2020-05-29 22:15:30 -0500

    [Feature] Adding 4 task 0x1A

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,11 +1,28 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get -
- *
- * Return: 
+ * hash_table_get - 1 if it succeeded, 0 otherwise
+ * @ht: hs the hash table you want to look into
+ * @key: is the key you are looking for
+ * Return: the value associated with the element,
+ * or NULL if key couldn’t be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned int i;
+	hash_node_t *crrnt;
 
+	if (!ht || !key || !*key)
+		return (NULL);
+
+	i = key_index((unsigned char *)key, ht->size);
+	crrnt = ht->array[i];
+
+	while (crrnt)
+	{
+		if (!strcmp(key, crrnt->key))
+			return (crrnt->value);
+		crrnt = crrnt->next;
+	}
+	return (NULL);
 }
