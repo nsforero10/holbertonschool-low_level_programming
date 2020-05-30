035a4f795e84742d916d10900a3cac39e53ce54c
Nicolas Forero <ns.forero10@gmail.com>
2020-05-29 22:05:27 -0500

    [Feature] Adding 0-3 tasks 0x1A

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,9 +1,11 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set -
- *
- * Return: 
+ * hash_table_set - adds an element to the hash table.
+ * @ht: is the hash table you want to add or update the key/value to
+ * @key: is the key. key can not be an empty string
+ * @value: is the value associated with the key.
+ * Return: 1 if it succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
