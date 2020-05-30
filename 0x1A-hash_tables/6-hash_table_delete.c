541dbdd76b1eebbd9320c08e3787d4ffef38cbb9
Nicolas Forero <ns.forero10@gmail.com>
2020-05-29 22:24:46 -0500

    [Feature] Adding 6 task 0x1A

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,11 +1,33 @@
 #include "hash_tables.h"
 
+
 /**
- * hash_table_delete -
- *
- * Return: 
+ * hash_table_delete - deletes a hash table.
+ * @ht: Hhash table
+ * Return: nothing
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	hash_table_t *h = ht;
+	hash_node_t *node, *tmp;
+	unsigned long int i = 0;
 
+	while (i < h->size)
+	{
+		if (h->array[i] != NULL)
+		{
+			node = h->array[i];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
+		}
+		i++;
+	}
+	free(ht->array);
+	free(ht);
 }
