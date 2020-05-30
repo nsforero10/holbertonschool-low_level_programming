18f2a3eab79e5af08b2c3a98171e9924ff2275ee
Nicolas Forero <ns.forero10@gmail.com>
2020-05-29 22:19:42 -0500

    [Feature] Adding 4 task 0x1A

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,11 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_print -
- *
- * Return: 
+ * hash_table_print - prints a hash table
+ * @ht: is the hash table
+ * Return: nothing
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int i = 0, n_el = 0;
+	hash_node_t *crrnt;
 
+	if (!ht)
+		return;
+
+	printf("{");
+	while (i < ht->size)
+	{
+		crrnt = ht->array[i];
+		while (crrnt)
+		{
+			if (n_el++ > 0)
+				printf(", ");
+			printf("'%s': '%s'", crrnt->key, crrnt->value);
+			crrnt = crrnt->next;
+		}
+		i++;
+	}
+	printf("}\n");
 }
