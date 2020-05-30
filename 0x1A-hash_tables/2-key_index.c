9e19d1fb0e3d648a12ec26b6907c9007bddb012d
Nicolas Forero <ns.forero10@gmail.com>
2020-05-29 22:03:50 -0500

    [Feature] Adding 0-3 tasks 0x1A

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,10 +1,11 @@
 #include "hash_tables.h"
 
 /**
- * key_index -
+ * key_index - gives you the index of a key.
  * @key: is the key
  * @size: the size of the array of the hash table
- * Return: the index at which the key/value pair should be stored in the array of the hash table
+ * Return: the index at which the key/value pair should be
+ * stored in the array of the hash table
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
