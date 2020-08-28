f1f370631389906f863655a4470bb04228bae3c6
Nicolas Forero <ns.forero10@gmail.com>
2020-08-27 21:18:00 -0500

    Update: adding tasks files and task 0 0x1E

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,24 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for valuue in an array of integers
+ * using linear search algorith
+ * @array: array of integers
+ * @size: the length of the array
+ * @value: the falue to search for
+ * Return: index of the first occurrency of the value in the array
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (!array || size == 0)
+		return (-1);
+	for (i = 0, i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
